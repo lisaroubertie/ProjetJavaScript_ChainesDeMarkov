0a9faddbe41263d59c20b7d8c73ae082252e0b36
lisaroubertie <lisa.roubertie@gmail.com>
2026-04-16 22:05:07 +0200

    Mise en place de probability + séparation lettres et mots

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import * as R from 'ramda';
 import {text} from '../data/text.js';
 import {normalizeText} from "./model/normalize.js";
-import {toPair} from "./model/count.js";
 import {count} from "./model/count.js";
+import {returnProba} from "./model/probability.js";
 
-console.log(count(normalizeText(text).split("")));
+console.log("--- Avec des lettres ---")
+const letters = normalizeText(text).split("");
+console.log(returnProba(count(letters)));
+
+console.log("--- Avec des mots ---")
+const words = normalizeText(text).split(" ");
+console.log(returnProba(count(words)));
--- src/model/probability.js
+++ src/model/probability.js
@@ -1,1 +1,11 @@
-// fait les probas
+import * as R from 'ramda';
+
+export const total = R.pipe(
+  R.values,
+  R.sum
+);
+
+export const probability = objet =>
+  R.map(x => x / total(objet), objet);
+
+export const returnProba = R.map(probability);
